332b5b45759be52ee4c6ad0ef853702bfa83c97a
Raúl Tamayo <261541580+raultamayoverdecia-hue@users.noreply.github.com>
2026-05-02 16:24:46 -0300

    media_aluno.py

---
+++ soma_dobro.py
@@ -0,0 +1,9 @@
+# Lê dois números, calcula a soma e o dobro dessa soma
+n1 = float(input("Digite o primeiro valor: "))
+n2 = float(input("Digite o segundo valor: "))
+
+soma = n1 + n2
+dobro = soma * 2
+
+print(f"A soma é: {soma}")
+print(f"O dobro da soma é: {dobro}")
